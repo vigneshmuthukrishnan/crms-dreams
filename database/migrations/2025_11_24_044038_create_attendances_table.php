d22b08c7bf22a38449d45cd423a3ea47a483dc8e
vignesh <info.vigneshmuthukrishnan@gmail.com>
2025-11-24 12:09:40 +0530

    New Changes

---
+++ database/migrations/2025_11_24_044038_create_attendances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->time('login_time')->nullable();
+            $table->time('logout_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
